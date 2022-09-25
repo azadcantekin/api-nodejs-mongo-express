6e555f407bd5339c88ee064a43fceb4414e1e5fe
Azad Can Tekin <tekin.act@gmail.com>
2022-09-25 07:06:40 +0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,13 @@ mongoose
     .connect(config.mongo.url, {retryWrites: true, w: 'majority'})
     .then(() => {
         Logging.info('Connected MongoDB');
+        StartServer();
     })
     .catch((error) => {
         Logging.error('Unable connection');
         Logging.error(error);
     });
+
+const StartServer = () => {
+    router.use((req, res, next) => {});
+};
