a30bb51355e102e677c720e87b994f593c045e5f
Azad Can Tekin <tekin.act@gmail.com>
2022-09-25 19:19:12 +0300

    Firs commit

--- src/server.ts
+++ src/server.ts
@@ -18,5 +18,34 @@ mongoose
     });
 
 const StartServer = () => {
-    router.use((req, res, next) => {});
+    router.use((req, res, next) => {
+        Logging.info(`Incoming -> Method : [${req.method}] - Url : [${req.url}] - IP : [${req.socket.remoteAddress}]`);
+        res.on('finish', () => {
+            Logging.info(`Incoming -> Method : [${req.method}] - Url : [${req.url}] 
+             - IP : [${req.socket.remoteAddress}] - STATUS : [${res.statusCode}]`);
+        });
+        next();
+    });
+
+    router.use(express.urlencoded({extended: true}));
+    router.use(express.json());
+
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Headers', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+        next();
+    });
+    router.get('/ping', (req, res, next) => res.status(200).json({message: 'pong'}));
+
+    router.use((req, res, next) => {
+        const error = new Error('not found');
+        Logging.error(error);
+        return res.status(404).json({message: error.message});
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}.`));
 };
